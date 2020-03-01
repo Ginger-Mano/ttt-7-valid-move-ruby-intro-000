90be8d2705d1c82be23d27f075971fdada8d483f
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-03-01 20:12:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,21 @@
 def valid_move?(board, index)
-
-
+  toots = "cat"
+  if toots  
+    return 'blue'
+  else 
+    return 'red'
+  end
 end
+
+board = ["X", " ", "X", " ", "O", " ", " ", " ", " "]
+board[0]
+
+def position_taken?(board, index) # board = ["X", " ", "X", " ", "O", " ", " ", " ", " "] index = 0
+if board[index] == " " || board[index] == "" || board[index] == nil
+  return false
+else
+  return true
+  end   # code your #position_taken? method here!
+end
+
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
