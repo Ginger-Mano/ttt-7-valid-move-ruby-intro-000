74074438cfb1e9790d83e4f5ccad1b0125e43d36
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-03-01 20:43:23 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 def valid_move?(board, index)
   if position_taken?(board, index)
     return false
-  elsif 
+  elsif
     if not board[index].between?(0, 8)
-      return false  
+      return false
   else
     return true
   end
@@ -14,6 +14,6 @@ def position_taken?(board, index)
     return false
   else
     return true
-  end   
+  end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
